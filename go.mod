a3cb8aeb2331e5101559658c252fd43cb706c706
Andrew Stiles <andrew@reftrace.com>
2024-08-17 23:45:30 -0700

    add cobra and starlark

--- go.mod
+++ go.mod
@@ -2,6 +2,15 @@ module reft-go
 
 go 1.22
 
-require github.com/antlr4-go/antlr/v4 v4.13.1
+require (
+	github.com/antlr4-go/antlr/v4 v4.13.1
+	github.com/spf13/cobra v1.8.1
+)
 
-require golang.org/x/exp v0.0.0-20240604190554-fc45aab8b7f8 // indirect
+require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	go.starlark.net v0.0.0-20240725214946-42030a7cedce // indirect
+	golang.org/x/exp v0.0.0-20240604190554-fc45aab8b7f8 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
 	"reft-go/parser"
@@ -10,17 +12,38 @@ import (
 
 	// Adjust the import path based on your module name and structure
 	"github.com/antlr4-go/antlr/v4" // Ensure this import path is correct based on your setup
+	"github.com/spf13/cobra"
+	"go.starlark.net/starlark"
 )
 
+var rootCmd = &cobra.Command{
+	Use:   "reft [directory]",
+	Short: "Process .nf files in a directory",
+	Args:  cobra.ExactArgs(1),
+	Run:   run,
+}
+
+var checkCmd = &cobra.Command{
+	Use:   "check [nf_file] [checks_file]",
+	Short: "Check a .nf file using rules from a checks.nf file",
+	Args:  cobra.ExactArgs(2),
+	Run:   runCheck,
+}
+
+func init() {
+	rootCmd.AddCommand(checkCmd)
+}
+
 func main() {
-	debug.SetGCPercent(-1)
-	// Ensure the input directory path is provided as a command-line argument
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: go run main.go <path_to_directory>")
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
+}
 
-	dir := os.Args[1]
+func run(cmd *cobra.Command, args []string) {
+	debug.SetGCPercent(-1)
+	dir := args[0]
 
 	var wg sync.WaitGroup
 
@@ -49,21 +72,6 @@ func main() {
 
 	wg.Wait()
 }
-
-/*
-
-type TreeShapeListener struct {
-	*parser.BaseGroovyParserListener
-}
-
-func NewTreeShapeListener() *TreeShapeListener {
-	return new(TreeShapeListener)
-}
-
-func (tsl *TreeShapeListener) EnterEveryRule(ctx antlr.ParserRuleContext) {
-	fmt.Println(ctx.GetText())
-}
-*/
 
 func processFile(filePath string) {
 	input, err := antlr.NewFileStream(filePath)
@@ -100,5 +108,45 @@ func processFile(filePath string) {
 		_ = ast
 		//builder.VisitCompilationUnit(unit.(*parser.CompilationUnitContext))
 		//antlr.ParseTreeWalkerDefault.Walk(NewTreeShapeListener(), tree)
+	}
+}
+
+func runCheck(cmd *cobra.Command, args []string) {
+	nfFile := args[0]
+	checksFile := args[1]
+
+	// Read the .nf file
+	nfContent, err := ioutil.ReadFile(nfFile)
+	if err != nil {
+		log.Fatalf("Error reading .nf file: %v", err)
+	}
+
+	// Read the checks.nf file
+	checksContent, err := ioutil.ReadFile(checksFile)
+	if err != nil {
+		log.Fatalf("Error reading checks.nf file: %v", err)
+	}
+
+	// Create a new Starlark thread
+	thread := &starlark.Thread{Name: "check_thread"}
+
+	// Create predefined variables for the Starlark environment
+	predefined := starlark.StringDict{
+		"nf_content": starlark.String(string(nfContent)),
+		// Add any other predefined variables or functions here
+	}
+
+	// Execute the checks.nf file
+	globals, err := starlark.ExecFile(thread, checksFile, checksContent, predefined)
+	if err != nil {
+		log.Fatalf("Error executing checks.nf: %v", err)
+	}
+
+	// Print the results
+	fmt.Println("Check results:")
+	for name, value := range globals {
+		if name != "nf_content" { // Skip printing the input content
+			fmt.Printf("%s = %v\n", name, value)
+		}
 	}
 }
