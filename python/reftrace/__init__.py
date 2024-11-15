5e3f8027a50d8bb126b9e3d00dadb67ac924d197
Andrew Stiles <andrew@reftrace.com>
2024-11-15 01:35:15 -0800

    fix circular import

--- python/reftrace/__init__.py
+++ python/reftrace/__init__.py
@@ -2,6 +2,5 @@ from .bindings.module import Module
 from .bindings.process import Process
 from .bindings.container import Container
 from .bindings.label import Label
-from . import linting
 
-__all__ = ['Module', 'Process', 'Container', 'Label', 'linting']
+__all__ = ['Module', 'Process', 'Container', 'Label']
